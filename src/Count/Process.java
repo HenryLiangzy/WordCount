7aef352fa9989120e17260c27d36daa68b24fd65
Henry Liang <Uranus.zy@gmail.com>
2018-04-07 21:46:08 +0800

    Add code

--- src/Count/Process.java
+++ src/Count/Process.java
@@ -1,4 +1,28 @@
 package Count;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class Process {
+    public static void main(String[] args){
+
+        FileInputStream input_file = null;
+        try{
+            input_file = new FileInputStream("NovelTexts.txt");
+            System.out.println("Import file successful");
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+
+
+        try{
+            input_file.close();
+            System.out.println("Close the file successful");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
