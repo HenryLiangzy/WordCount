210f8b662a236142eab8e9822604a62d805e7967
Henry Liang <Uranus.zy@gmail.com>
2018-04-10 22:53:28 +0800

    Add the new class

--- src/Count/Process.java
+++ src/Count/Process.java
@@ -7,10 +7,10 @@ import java.io.*;
 public class Process {
     public static void main(String[] args){
 
-        File in = new File("NovelTexts.txt");
+        File in = new File("test.txt");
         FileInputStream input_file = null;
         try{
-            input_file = new FileInputStream("NovelTexts.txt");
+            input_file = new FileInputStream("test.txt");
             System.out.println("Import file successful");
 
         }catch (IOException e){
@@ -20,6 +20,14 @@ public class Process {
         long length = in.length();
         System.out.println("The length of file is:" + length);
 
+        try{
+            int data;
+            while ((data = input_file.read()) != -1){
+                
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
 
 
         try{
