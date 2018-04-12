b898d33b650e70d2f445192af1b729ac5b938458
Henry Liang <Uranus.zy@gmail.com>
2018-04-12 13:08:30 +0800

    Edit the Express class and regular the expression

--- src/Count/Express.java
+++ src/Count/Express.java
@@ -3,14 +3,25 @@ package Count;
 import java.util.Scanner;
 
 public class Express {
+
+    //edit the regular express
     public String editExpress(String original_String){
         String output_String;
         String normal = "(s)(ing)(ness)(ed)(ly)";
 
         char head = original_String.charAt(0);
 
+        char output_char = ' ';
 
-        output_String = original_String.substring(1) + normal;
+        if (Character.isUpperCase(head)){
+             output_char = Character.toLowerCase(head);
+        }
+        else if (Character.isLowerCase(head)){
+            output_char = Character.toUpperCase(head);
+        }
+
+
+        output_String ="(" + Character.toString(head) + ")(" + Character.toString(output_char) + ")" +  original_String.substring(1) + normal;
 
         return output_String;
     }
