d83ea31b10498dba0257fa57c356fc426503333f
Henry Liang <Uranus.zy@gmail.com>
2018-04-13 15:10:31 +0800

    First version of the WordCount

--- src/Count/MatchWord.java
+++ src/Count/MatchWord.java
@@ -1,7 +1,11 @@
 package Count;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MatchWord {
@@ -10,18 +14,34 @@ public class MatchWord {
 
     //test for the regular expression
     public static int main(FileInputStream input_file){
-        int times = 0;
 
+        //Create the BufferedReader object
+        BufferedReader bf_file = new BufferedReader(new InputStreamReader(input_file));
+
+        //Construct the regular expression
         System.out.println("Input the match word:");
         Scanner keyboard_input = new Scanner(System.in);
         String input_phrase = keyboard_input.nextLine();
 
-
         Express expression = new Express();
         Pattern pattern = Pattern.compile(expression.editExpress(input_phrase));
+        Matcher matcher = null;
 
+        int times = 0;
+        String read_line = null;
 
+        try{
+            while ((read_line = bf_file.readLine()) != null){
+                matcher = pattern.matcher(read_line);
+                while (matcher.find()){
+                    times++;
+                }
+            }
 
+            bf_file.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
 
         return times;
     }
