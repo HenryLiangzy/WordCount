452bebc2e41c1962f2fdf5549248181d99f55e9a
Henry Liang <Uranus.zy@gmail.com>
2018-04-26 13:03:35 +0800

    Fix the stemming class, improve recognition rate

--- src/Stemming/Search.java
+++ src/Stemming/Search.java
@@ -1,4 +1,9 @@
 package Stemming;
 
+import java.io.FileInputStream;
+
 public class Search {
+    public static void searchWord(FileInputStream input_file){
+
+    }
 }
--- src/Stemming/Stemmer.java
+++ src/Stemming/Stemmer.java
@@ -7,11 +7,22 @@ public class Stemmer {
     public static String stemmingWord(String input_word){
         String output_word = null;
 
-        Pattern pattern = Pattern.compile("\\b(s|ing|ness|ed|ly)*\\b");
+        Pattern pattern = Pattern.compile("([a-zA-Z]+)(ness|ing|ed|ly|s)+");
         Matcher matcher = pattern.matcher(input_word);
+
+        if (matcher.find()){
+            output_word = matcher.replaceAll(matcher.group(1));
+        }
+
+
 
         return output_word;
     }
 
+    public static void main(String[] args){
+        String test = "fuckness";
+
+        System.out.println(stemmingWord(test));
+    }
 
 }
