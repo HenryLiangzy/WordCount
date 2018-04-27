84c4839355a2cb9c074649a6c11d39bb12bfdf9f
Henry Liang <Uranus.zy@gmail.com>
2018-04-28 01:36:40 +0800

    Add process display function

--- src/Stemming/Process.java
+++ src/Stemming/Process.java
@@ -28,23 +28,24 @@ public class Process {
 
         //Calculate the file size
         long length = in.length();
-        System.out.println("The length of file is:" + length);
+        System.out.println("The length of file is: " + length + " byte");
 
         BufferedReader bf_file = new BufferedReader(new InputStreamReader(input_file));
 
         List<item> wordList = new ArrayList<item>();
         String read_line = null;
-        List<String> wordTemp = new ArrayList<String>();
         long costTime_start = System.currentTimeMillis();
 
         //main process step
+        long finish = -1;
         try{
             while ((read_line = bf_file.readLine()) != null){
 
-
+                finish = finish + read_line.length() + 1;
+                System.out.print("\rFinish: " + (finish*100/length) + "%");
                 wordList = Search.searchWord(Spilt.replaceSymbols(read_line), wordList);
-
             }
+
 
             bf_file.close();
             input_file.close();
@@ -55,7 +56,7 @@ public class Process {
 
         long costTime_end = System.currentTimeMillis();
         long total = costTime_end - costTime_start;
-        System.out.println("Total cost: " + total + " ms");
+        System.out.println("\nTotal cost: " + total + " ms, " + finish + " byte");
 
         System.out.print("Input s to print the result on screen or f export the result file, or b both of them:");
         String decide = input_keyboard.nextLine();
