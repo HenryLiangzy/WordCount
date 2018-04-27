f6d652c8b559acb83b09a7f57552dca56e4322db
Henry Liang <Uranus.zy@gmail.com>
2018-04-27 14:10:57 +0800

    Add option task, allow user choose file and output ways

--- src/Stemming/Print.java
+++ src/Stemming/Print.java
@@ -1,5 +1,7 @@
 package Stemming;
 
+import java.io.*;
+import java.util.Calendar;
 import java.util.List;
 
 public class Print {
@@ -15,7 +17,34 @@ public class Print {
 
     }
 
-    public static void printFile(List<item> wordList){
+    public static void printFile(List<item> wordList, long totalCost){
 
+        Calendar current_time = Calendar.getInstance();
+
+        int hour = current_time.get(Calendar.HOUR_OF_DAY);
+        int minute = current_time.get(Calendar.MINUTE);
+        int second = current_time.get(Calendar.SECOND);
+
+        String time_string = Integer.toString(hour) + "_" + Integer.toString(minute) + "_" + Integer.toString(second);
+
+        try{
+            File out = new File("WordCount_" + time_string + ".txt");
+            OutputStream output_file = new FileOutputStream(out);
+            OutputStreamWriter output_line = new OutputStreamWriter(output_file, "UTF-8");
+
+            for (int i = 0; i < wordList.size(); i++){
+                output_line.write(wordList.get(i).getWord() + " " + wordList.get(i).getTimes() + "\n");
+            }
+
+            output_line.write("Total cost " + totalCost + " ms to WordCount\n");
+
+            output_line.close();
+            output_file.close();
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        System.out.println("Export the result file successful");
     }
 }
--- src/Stemming/Process.java
+++ src/Stemming/Process.java
@@ -9,8 +9,12 @@ public class Process {
 
     public static void main(String[] args){
 
+        Scanner input_keyboard = new Scanner(System.in);
+        System.out.print("Input the novel path:");
+        String file_path = input_keyboard.nextLine();
+
         //Create the file object
-        File in = new File("test.txt");
+        File in = new File(file_path);
         FileInputStream input_file = null;
 
         //read the file from the disk
@@ -53,9 +57,22 @@ public class Process {
         long total = costTime_end - costTime_start;
         System.out.println("Total cost: " + total + " ms");
 
+        System.out.print("Input s to print the result on screen or f export the result file, or b both of them:");
+        String decide = input_keyboard.nextLine();
 
-
-        Print.printScreen(wordList);
+        if (decide.equalsIgnoreCase("s")){
+            Print.printScreen(wordList);
+        }
+        else if (decide.equalsIgnoreCase("f")){
+            Print.printFile(wordList, total);
+        }
+        else if(decide.equalsIgnoreCase("b")){
+            Print.printScreen(wordList);
+            Print.printFile(wordList, total);
+        }
+        else {
+            System.out.println("Invalid input");
+        }
 
     }
 }
